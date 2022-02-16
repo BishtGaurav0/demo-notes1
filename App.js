de8a1dd73ef642269e9e07299ec2aede033b5744
BishtGaurav0 <bisht1999gaurav@gmail.com>
2022-02-16 20:30:39 +0530

    first code

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Notes from './Component/Notes';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Notes/>
+    </div>
+  );
+}
+
+export default App;
